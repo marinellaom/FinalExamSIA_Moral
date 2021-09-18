66e1d10bd638ad9fd2d2854d9573011d9e4258e8
Marinella Moral <moral1820393@mkt.ceu.edu.ph>
2021-09-18 08:30:31 +0800

    upload all

---
+++ app/Http/Controllers/AppController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Facade\FlareClient\Http\Response;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+use GuzzleHttp\Client;
+
+
+class AppController extends Controller
+{
+    
+    public function list(Request $request){
+
+      $token = "AoLAQIfceOZvTL8dQLgLDZkQzr3ONVBY";
+      $value1 = $request->input("q");
+      $value2 = $request->input("language");
+
+      if(is_null($request)){
+        $data = Http::get("http://dataservice.accuweather.com/locations/v1/cities/search?apikey=$token&q=Manila&language=en-us&details=false")->json();
+        return view("welcome", ['data'=>$data]);
+      }
+
+      
+      $data = Http::get("http://dataservice.accuweather.com/locations/v1/cities/search?apikey=$token&q=$value1&language=$value2&details=false")->json();
+      
+      return view("welcome", ['data'=>$data]);
+    }
+}
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Final Exam - Post Task</title>
+  <link rel="stylesheet" href="css/style.css">
+
+  <script src="{{ asset('js/app.js') }}" defer></script>
+   
+   <!-- Styles -->
+   <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body class="bg-white">
+  <div class="container bg-opacity-dark p-5">
+    <div class="header border border-dark bg-opacity-tan p-5 mb-5 mx-4">
+      <h1 class="text-center">Final Exam - API </h1>
+      <h5 class="text-center">Moral, Marinella O. </h5>
+      <h6 class="text-center">BSIT - 4A </h6>
+
+
+    </div>
+    <div class="content border border-dark bg-opacity-tan p-5 my-5 mx-4">
+      <div class="user-input text-center bg-opacity-secondary p-3">
+        <form action="/" method="get">
+          @csrf
+          <div class="row m-2">
+            <div class="col-sm-6"><label for="value1">City</label></div>
+            <div class="col-sm-6"><label for="value1">Language</label></div>
+          </div>
+          <div class="row m-2">
+            <div class="col-sm-6"><input type="text" name="q" id="" placeholder="Input City" value="Manila"></div>
+            <div class="col-sm-6"><input type="text" name="language" id="" placeholder="en-us" value="en-us"></div>
+          </div>
+    
+          <button type="submit" class="mt-3 btn btn-info">Submit</button>
+        </form>
+      </div>
+    </div>
+
+    <div class="border border-dark bg-opacity-tan p-5 my-5 mx-4">
+      <div class="results text-center bg-opacity-secondary p-3">
+        <div class="row">
+          <div class="col-sm-12"><h3>Search Results</h3></div>
+        </div>
+        <div class="row border-dark m-4">
+          @isset($data)
+          <div class="col-3"><h4>Country</h4></div>
+          <div class="col-9">
+            {{print_r($data[0]["Country"]["LocalizedName"])}}
+          </div>
+          <div class="col-3"><h4>World Position</h4></div>
+            <div class="col-9">
+
+              <div class="row">
+                <div class="col-4">
+                  <p>Latitude</p>{{print_r($data[0]["GeoPosition"]["Latitude"])}}<br>
+                </div>
+                <div class="col-4">
+                  <p>Longitude</p>{{print_r($data[0]["GeoPosition"]["Longitude"])}}
+                </div>
+              </div>
+            </div>
+          </div>
+
+          @endisset
+        </div>
+      </div>
+    </div>
+  </div>
+</body>
+</html>
