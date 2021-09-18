71ca4be241fb3f4b2b0869df9edaacf6cc102708
Marinella Moral <moral1820393@mkt.ceu.edu.ph>
2021-09-18 08:39:16 +0800

    fix error

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\MathController;
+use App\Http\Controllers\AppController;
 
 /*
 |--------------------------------------------------------------------------
